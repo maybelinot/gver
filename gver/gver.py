9d09d59ce9043550e277b19d095c8199eacd143d
etrott <etrott@redhat.com>
2015-10-06 15:57:27 +0200

    Add basic functionality

--- gver/gver.py
+++ gver/gver.py
@@ -4,7 +4,7 @@
 # @Date:   2015-09-16 11:28:21
 # @Email:  etrott@redhat.com
 # @Last modified by:   etrott
-# @Last Modified time: 2015-09-17 14:09:00
+# @Last Modified time: 2015-10-06 15:53:46
 
 
 import os
@@ -12,28 +12,67 @@ import sys
 from string import ascii_uppercase
 import httplib2
 
+from apiclient import errors
 from apiclient import discovery
+from apiclient.http import MediaIoBaseUpload
+from StringIO import StringIO
 
 from utils import logr
 
-try:
-    input = raw_input
-except NameError:  # Python 3
-    pass
 
+def update_file(service, file_id, media_body):
+    """Update an existing file's metadata and content.
 
-def history(credentials=None, file_id=None):
-    '''
-    FIXME DOCs
-    '''
-    http = credentials.authorize(httplib2.Http())
-    # FIXME: Different versions have different keys like v1:id, v2:fileId
-    service = discovery.build('drive', 'v2', http=http)
-
+    Args:
+      service: Drive API service instance.
+      file_id: ID of the file to update.
+      new_title: New title for the file.
+      new_description: New description for the file.
+      new_mime_type: New MIME type for the file.
+      new_filename: Filename of the new content to upload.
+      new_revision: Whether or not to create a new revision for this file.
+    Returns:
+      Updated file metadata if successful, None otherwise.
+    """
     try:
-        revisions = service.revisions().list(fileId=file_id).execute()
-        return revisions.get('items', [])
+        # First retrieve the file from the API.
+        file = service.files().get(fileId=file_id).execute()
+        print(file)
+        # File's new content.
+        # media_body = MediaFileUpload(
+        #     new_filename, mimetype=new_mime_type, resumable=True)
+
+        # Send the request to the API.
+        updated_file = service.files().update(
+            fileId=file_id,
+            body=file,
+            newRevision=True,
+            media_body=media_body).execute()
+        return updated_file
     except errors.HttpError, error:
         print 'An error occurred: %s' % error
-    return None
+        return None
 
+
+def get_file_content(http, file_id):
+    """Print a file's content.
+
+    Args:
+      service: Drive API service instance.
+      file_id: ID of the file.
+
+    Returns:
+      File's content if successful, None otherwise.
+    """
+    try:
+        service = discovery.build('drive', 'v2', http=http)
+
+        url = service.files().get(fileId=file_id).execute()[
+            'exportLinks']['text/html']
+        response, content = http.request(url)
+        print(content)
+        media_body = MediaIoBaseUpload(
+            StringIO(content), 'text/html', resumable=False)
+        return media_body
+    except errors.HttpError, error:
+        print 'An error occurred: %s' % error
--- gver/utils.py
+++ gver/utils.py
@@ -4,7 +4,7 @@
 # @Date:   2015-09-11 10:57:06
 # @Email:  etrott@redhat.com
 # @Last modified by:   etrott
-# @Last Modified time: 2015-09-17 14:03:49
+# @Last Modified time: 2015-10-06 12:44:56
 
 from __future__ import unicode_literals, absolute_import
 
@@ -13,19 +13,39 @@ import os
 import subprocess
 import sys
 
-import oauth2client
+import yaml
+from oauth2client import file, client, tools
 
 # Load logging before anything else
 logging.basicConfig(format='>> %(message)s')
 logr = logging.getLogger('members')
 
+# History file
+CONFIG_FILE = os.path.expanduser('~/.gver')
+if os.path.exists(CONFIG_FILE):
+    with open(CONFIG_FILE) as _:
+        config = yaml.load(_)
+    if config is None:
+        config = {}
+else:
+    config = {}
+
+
 ''' Load the file with credentials '''
 CLIENT_SECRET_FILE = os.path.expanduser('~/.gdrive_private')
+
+DEFAULT_TOKEN = os.path.expanduser('~/.oauth/drive.json')
 
 SCOPES = ('https://www.googleapis.com/auth/drive.metadata.readonly '
           'https://www.googleapis.com/auth/drive '
           'https://spreadsheets.google.com/feeds '
           'https://docs.google.com/feeds')
+
+
+def update_config(dct):
+    with open(CONFIG_FILE, "w") as _:
+        yaml.dump(dct, _)
+
 
 def run(cmd):
     cmd = cmd if isinstance(cmd, list) else cmd.split()
@@ -54,29 +74,24 @@ def get_credentials():
     try:
         import argparse
         flags = argparse.ArgumentParser(
-            parents=[oauth2client.tools.argparser]).parse_known_args()[0]
+            parents=[tools.argparser]).parse_known_args()[0]
     except ImportError:
         flags = None
         logr.error(
-            'Unable to parse oauth2client args; `pip install argparse` required')
+            'Unable to parse oauth2client args; `pip install argparse`')
 
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'drive.json')
+    store = file.Storage(DEFAULT_TOKEN)
 
-    store = oauth2client.file.Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
 
-        flow = oauth2client.client.flow_from_clientsecrets(
+        flow = client.flow_from_clientsecrets(
             CLIENT_SECRET_FILE, SCOPES)
-        flow.redirect_uri = oauth2client.client.OOB_CALLBACK_URN
+        flow.redirect_uri = client.OOB_CALLBACK_URN
         if flags:
-            credentials = oauth2client.tools.run_flow(flow, store, flags)
+            credentials = tools.run_flow(flow, store, flags)
         else:  # Needed only for compatability with Python 2.6
-            credentials = oauth2client.tools.run(flow, store)
-        logr.info('Storing credentials to ' + credential_path)
+            credentials = tools.run(flow, store)
+        logr.info('Storing credentials to ' + DEFAULT_TOKEN)
+
     return credentials
